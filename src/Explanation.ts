64def1b4740d12ad769003f4053561a3a3b99715
Philip Johnson <p.zhao.johnson@gmail.com>
2024-06-30 14:53:18 -0400

    Adjust styles and text contents

--- src/Explanation.ts
+++ src/Explanation.ts
@@ -1,16 +1,27 @@
 import * as styles from './Explanation.css';
 
+function BoldSpan(textContent: string) {
+  let boldSpan = document.createElement('span');
+  boldSpan.textContent = textContent;
+  boldSpan.style.fontWeight = '700';
+  return boldSpan;
+}
+
 export function Explanation() {
   let explanation = document.createElement('div');
 
+  let SVG = BoldSpan('SVG');
+  let AdobeIllustrator = BoldSpan('Adobe Illustrator');
+  let Inkscape = BoldSpan('Inkscape');
+
   let topLine = document.createElement('p');
   topLine.classList.add(styles['explanation-text']);
-  topLine.textContent = 'Export the drawing as an SVG image.';
+  topLine.append('Export the drawing as an ', SVG, ' image.');
   explanation.append(topLine);
 
   let bottomLine = document.createElement('p');
   bottomLine.classList.add(styles['explanation-text']);
-  bottomLine.textContent = 'SVG images can be opened, edited further and converted to other image formats using vector graphics softwares such as Adobe Illustrator and Inkscape.';
+  bottomLine.append('SVG images can be opened, edited further and converted to other image formats using vector graphics softwares such as ', AdobeIllustrator, ' and ', Inkscape, '.');
   explanation.append(bottomLine);
 
   return explanation

0483014df3a852b6828f6c58a892d59e3cfa5dfd
Philip Johnson <p.zhao.johnson@gmail.com>
2024-06-30 13:44:38 -0400

    Set up repository to be an npm package

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                namedExport: true,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    globalObject: 'this',
+    library: {
+      name: 'forms.export',
+      type: 'umd',
+    },
+    clean: true,
+  },
+};
